8c7d6c6e2bab840114bf96bf2332e51021523b21
jdelacruz96 <jonathan.delacruz@revature.com>
2023-05-19 14:48:27 -0400

    figuring out imocha auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string;
+  password: string;
 
+  constructor() {
+    this.username = '';
+    this.password = '';
+  }
+
+  login() {
+    // Perform login logic here
+    if (this.username === 'admin' && this.password === 'password') {
+      // Successful login, redirect to dashboard
+      console.log('Login successful!');
+    } else {
+      // Invalid credentials, display error message
+      console.log('Invalid credentials');
+    }
+  }
 }
--- src/app/test-list/test-list.component.ts
+++ src/app/test-list/test-list.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-test-list',
   templateUrl: './test-list.component.html',
   styleUrls: ['./test-list.component.css']
 })
-export class TestListComponent {
+export class TestListComponent implements OnInit {
+  tests: any[];
+  itemsPerPageOptions: number[] = [5, 10, 20];
+  itemsPerPage: number = 5;
+  currentPage: number = 1;
 
+  constructor(private http: HttpClient) {
+    this.tests = [];
+  }
+
+  ngOnInit() {
+    this.getTests();
+  }
+
+  getTests() {
+    const url = 'https://apiv3.imocha.io/v3/tests';
+    const headers = new HttpHeaders()
+      .set('x-api-key', 'FEsStirljsXozgHSxDUFZpdLFDspdv');
+    
+
+    this.http.get<any[]>(url, { headers }).subscribe(
+      (response) => {
+        this.tests = response;
+      },
+      (error) => {
+        console.log('Error occurred while fetching tests:', error);
+      }
+    );
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.getTests();
+  }
 }
