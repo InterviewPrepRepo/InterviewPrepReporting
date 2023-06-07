37650cd3675aa8329faa9bd3c24fa7d7d3cf3f35
Marielle Nolasco <marielletnolasco@gmail.com>
2023-06-07 11:39:40 -0400

    created custom carousel

--- src/app/question-carousel/question-carousel.component.ts
+++ src/app/question-carousel/question-carousel.component.ts
@@ -8,11 +8,20 @@ import { QuestionsService } from '../services/questions-service/questions.servic
   styleUrls: ['./question-carousel.component.css']
 })
 export class QuestionCarouselComponent implements OnInit {
+  currentSlide: number = 0;
   ngOnInit(): void {
     this.sampleQuestions = this.questions.getQuestions();
   }
   constructor(private questions: QuestionsService) { }
   sampleQuestions: Question[] = []
 
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.currentSlide : previous;
+  }
 
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.sampleQuestions.length ? this.currentSlide : next;
+  }
 }
